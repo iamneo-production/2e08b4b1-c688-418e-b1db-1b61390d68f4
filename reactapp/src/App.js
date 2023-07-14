8d7d538ab94e6c47df6b0fab63241c7d66b261b5
JISHNUCAIRO <727721eucs050@skcet.ac.in>
2023-07-14 10:04:06 +0000

    thanks

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Log from './Pages/Login'; 
+import { SocialIcon } from 'react-social-icons';
+import signup from './Pages/Signup';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Hom from './Components/home';
+import Mar from './Components/marketing';
+import Abo from './Components/about';
+import Sports from './Components/sports';
+import Tech from './Components/technology';
+import Enter from './Components/entertainment';
+import Poli from './Components/politics';
+import Pro from './Components/profile';
+import { useLocation } from 'react-router-dom';
+import Nav from './pagebars/nav';
+import Side from './pagebars/sidebar';
+import Footer from './pagebars/footer';
+const Fix =()=>{
+const location = useLocation();
+if(location.pathname!='/' && location.pathname!='/signup')
+  {
+    return(
+      <>
+        <Nav/>
+        
+      </>
+    )
+ }
 
+  
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Fix/>
+        <Routes>
+          <Route path="/" Component={Log} />
+          <Route path="/signup" Component={signup} />
+          <Route path="/h" Component={Hom} />
+          <Route path="/profile" Component={Pro} />
+          <Route path="/market" Component={Mar} />
+          <Route path="/about" Component={Abo} />
+          <Route path="/techno" Component={Tech} />
+          <Route path="/enter" Component={Enter} />
+          <Route path="/poli" Component={Poli} />
+          <Route path="/sports" Component={Sports} />
+        </Routes>
+      </Router>
     </div>
   );
 }
